2f38661bbf5559a16e83384052333de97ca2a8e0
Ian Ndolo Mwau <134711923+Ian386@users.noreply.github.com>
2024-06-07 13:06:35 +0300

    created models and altered sttings kidogo

--- fieldmapping/models.py
+++ fieldmapping/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.gis.db import models as gis_models
+
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    label = models.CharField(max_length=100)
+    location = gis_models.PointField(srid=4326)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Locations'
+
+class Farm(models.Model):
+    name = models.CharField(max_length=100)
+    farm_area =gis_models.PolygonField(srid=4326)
+    description = models.TextField(blank=True, null=True)
+    region = models.CharField(max_length=100)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='farms')
+    
+
+    def _str_(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Farms'
+
+
+class Crop(models.Model):
+    name = models.CharField(max_length=100)
+    type= models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    farm = models.ForeignKey(Farm, on_delete=models.CASCADE, related_name='crops')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Crops'   
