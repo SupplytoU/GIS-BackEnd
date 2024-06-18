61a8590719a0c94c76ddc8ed585687ee339db362
David Mwalimu <nzambulidavid@gmail.com>
2024-06-18 12:49:47 +0300

    Overrode the change_list for farms do display a map with all the registered farms, modified the Location view to allow querying using region

--- fieldmapping/admin.py
+++ fieldmapping/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from leaflet.admin import LeafletGeoAdmin
 from django.utils import timezone
+from django.core.serializers import serialize
 from .models import Location, Farm, Farmer, Produce
 
 
@@ -8,6 +9,8 @@ class CustomLocationAdmin(LeafletGeoAdmin, admin.ModelAdmin):
     readonly_fields = ('date_created',)
     change_list_template = 'admin/fieldmapping/location/location_change_list.html'
     change_form_template = 'admin/fieldmapping/location/change_form.html'
+    list_filter = ('region', 'label')
+
     def save_model(self, request, obj, form, change):
         if not obj.pk:
             obj.date_created = timezone.now()
@@ -38,6 +41,23 @@ class CustomFarmAdmin(LeafletGeoAdmin, admin.ModelAdmin):
     list_display = ('name', 'location', 'farmer')
     list_filter = ('produce', 'farmer')
     filter_horizontal = ('produce',)
+    change_list_template = 'admin/fieldmapping/farm/farm_change_list.html'
+
+    def changelist_view(self, request, extra_context=None):
+        farms = Farm.objects.all()
+        farm_data = []
+        for farm in farms:
+            # Assuming 'polygon' is the field name for the farm's polygon
+            geojson = farm.farm_area.geojson if farm.farm_area else None
+            farm_data.append({
+                'name': farm.name,
+                'geojson': geojson
+            })
+
+        extra_context = extra_context or {}
+        extra_context['farms'] = farm_data
+
+        return super().changelist_view(request, extra_context=extra_context)
 
 
 admin.site.register(Farm, CustomFarmAdmin)
